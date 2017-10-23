d2e0e9e07804c35b209d8c5b4c0ce613f5923368
rrfvtgb <geof.brule@laposte.net>
2017-10-23 14:15:30 +0200

    ~ Fix onglet

--- app/src/main/java/com/rrfvtgb/myapplication/MainActivity.java
+++ app/src/main/java/com/rrfvtgb/myapplication/MainActivity.java
@@ -56,16 +56,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            resetView();
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     Log.d("MainActivity", profil);
                     return true;
                 case R.id.navigation_result:
-
                     //readJSON();
                     return true;
                 case R.id.navigation_setup:
-                    resetView();
                     try {
                         vueProfil.afficher();
                     } catch (Exception e) {
@@ -126,8 +125,7 @@ public class MainActivity extends AppCompatActivity {
         //Set the text
 
         try {
-            JSONObject jObject = new JSONObject(result);
-            return jObject;
+            return new JSONObject(result);
         } catch (JSONException e) {
             e.printStackTrace();
         }
